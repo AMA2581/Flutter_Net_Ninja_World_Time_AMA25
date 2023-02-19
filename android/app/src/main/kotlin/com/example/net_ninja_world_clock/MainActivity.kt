48f1ee00d18c0c56513876fa4122c5f3b32377dc
AMA2581 <amir.abravesh.ama@gmail.com>
2023-02-19 21:52:46 +0330

    project creation

---
+++ android/app/src/main/kotlin/com/example/net_ninja_world_clock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.net_ninja_world_clock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Text('home screen')
+      ),
+    );
+  }
+}
